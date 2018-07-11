85fe938c6ad14e8678e0648c4bb11ab39a08e5cd
Robert Pratt <bpong@v8ch.com>
2018-07-11 08:20:05 +0700

    Cleanup and linting

--- v8ch-aws-cognito-authentication.php
+++ v8ch-aws-cognito-authentication.php
@@ -1,21 +1,16 @@
-<?php
+<?php // phpcs:ignore
 /**
  * Author:      Robert Pratt
  * Author URI:  https://www.v8ch.com/
  * Description: Authenticates against AWS Cognito.
- * Domain Path: /languages
  * License:     MIT
  * License URI: https://opensource.org/licenses/MIT
  * Plugin Name: V8CH AWS Cognito Authentication
- * Plugin URI:  https://github.com/V8CH/aws-cognito-authentication
- * Text Domain: aws-cognito-authentication
- * Version:     0.0.1
+ * Plugin URI:  https://github.com/V8CH/v8ch-aws-cognito-authentication
+ * Version:     1.0.0
  *
- * @category Authentication
- * @package  declaration
- * @author   Robert Pratt <bpong@v8ch.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://github.com/V8CH/aws-cognito-authentication
+ * @package aws-cognito-authentication
+ * @since   1.0.0
  */
 
 use V8CH\WordPress\AWSCognitoAuthentication\Plugin;
@@ -23,21 +18,14 @@ use V8CH\WordPress\AWSCognitoAuthentication\Plugin;
 require_once 'vendor/autoload.php';
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (! defined('ABSPATH')) {
+    exit;
 }
 
-define( 'V8CH_AWS_COGNITO_AUTHENTICATION_VERSION', '0.0.1' );
-
-/**
- * Start the plugin.
- *
- * @since    0.0.1
- */
-function run_v8ch_aws_cognito_authentication() {
-
-	$plugin = new Plugin();
-	$plugin->run();
-
+function run_v8ch_aws_cognito_authentication()
+{
+    $plugin = new Plugin();
+    $plugin->run();
 }
+
 run_v8ch_aws_cognito_authentication();
